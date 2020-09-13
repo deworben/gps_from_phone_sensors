c16b61a857508d043d7784b82825106f89a634a4
benjamin michaels <bdeworsop@gmail.com>
2020-09-13 18:29:32 +1000

    Added compass

--- Assets/AppManager.cs
+++ Assets/AppManager.cs
@@ -21,16 +21,39 @@ public class AppManager : MonoBehaviour
 
 
     public FilterButterworth fbwX = new FilterButterworth(15, 50, FilterButterworth.PassType.Highpass, (float)1.414);
-    public FilterButterworth findGX = new FilterButterworth(0, 2, FilterButterworth.PassType.Lowpass, (float)1.414);
-    public FilterButterworth findGY = new FilterButterworth(0, 1, FilterButterworth.PassType.Lowpass, (float)1.414);
-    public FilterButterworth findGZ = new FilterButterworth((float)0.2, 3, FilterButterworth.PassType.Lowpass, (float)1.414);
+    
+    public FilterButterworth findGX = new FilterButterworth((float)0.1, 50, FilterButterworth.PassType.Lowpass, (float)1.414);
+    public FilterButterworth findGY = new FilterButterworth((float)0.1, 50, FilterButterworth.PassType.Lowpass, (float)1.414);
+    public FilterButterworth findGZ = new FilterButterworth((float)0.1, 50, FilterButterworth.PassType.Lowpass, (float)1.414);
 
     public Pedometer pedometer = new Pedometer((float)0.05);
     public Gyro_first_attempt gyro_first_attempt = new Gyro_first_attempt((float)0.02);
     public Compass compass = new Compass();
-    public Kalman kalman = new Kalman();
+    public Kalman kalmanX = new Kalman();
+    public Kalman kalmanY = new Kalman();
+    public Kalman kalmanZ = new Kalman();
     public DataLogger dl = new DataLogger();
 
+                // projection of heading on 2-D plane (perpendicular to gravity)
+    public double twoDX;
+    public double twoDY;
+    public double twoDZ;
+
+    public double u_projection_magnitude;
+
+
+
+    public double orthogonal_x;
+    public double orthogonal_y;
+    public double orthogonal_z;
+
+    //Remove the component of u that is orthogonal to the plane
+    public double compass_needle_x;
+    public double compass_needle_y;
+    public double compass_needle_z;
+
+
+    public double heading;
 
     // Live graphs
     // public Window_Graph window = new Window_Graph();
@@ -94,8 +117,10 @@ public class AppManager : MonoBehaviour
                 gyro_first_attempt.init_gyro_values(compass.aboutX, compass.aboutY, compass.aboutZ);
                 gyro_first_attempt.isInitialised = true;
             }
-            //update kalman
-            kalman.update(gyro_first_attempt.orientationY, compass.aboutY);
+            //update kalmanX
+            kalmanX.update(gyro_first_attempt.orientationX, compass.aboutX);
+            kalmanY.update(gyro_first_attempt.orientationY, compass.aboutY);
+            kalmanZ.update(gyro_first_attempt.orientationZ, compass.aboutZ);
             
 
             // Display the data to GUI
@@ -104,7 +129,7 @@ public class AppManager : MonoBehaviour
                 "V1: Dist = " + 0.8* pedometer.numSteps + " meters \n" + 
                 "V2: Dist = " + pedometer.distance_travelled + " meters \n" + 
                 "max_pulse_magnitude = " + pedometer.max_pulse_magnitude + " \n" + 
-                "kalman at = " + kalman.xhat + " \n" + 
+                "kalmanX at = " + kalmanX.xhat + " \n" + 
                 "";
             disp_data += string.Format("Gyro data: x: {0:F4}, y: {1:F4}, z: {2:F4}.\n", Input.gyro.rotationRate.x, Input.gyro.rotationRate.y, Input.gyro.rotationRate.z);
             disp_data += string.Format("Compass data: x: {0:F4}, y: {1:F4}, z: {2:F4}.\n\n", Input.compass.rawVector.x, Input.compass.rawVector.y, Input.compass.rawVector.z);
@@ -114,10 +139,30 @@ public class AppManager : MonoBehaviour
             disp_data += string.Format("Gyro angles: x: {0:F4}, y: {1:F4}, z: {2:F4}.\n", gyro_first_attempt.orientationX, gyro_first_attempt.orientationY, gyro_first_attempt.orientationZ);
             disp_data += string.Format("Compass angles: x: {0:F4}, y: {1:F4}, z: {2:F4}. \n", compass.aboutX, compass.aboutY, compass.aboutZ);
             
+            // Create the unit vector with the same orientation as defined by kalman x,y,z
+            twoDX = Math.Cos(kalmanZ.xhat);
+            twoDY = Math.Sin(kalmanZ.xhat);
+            twoDZ = Math.Cos(kalmanX.xhat);
+
+            // Project this niggz onto a vactor perpendicular to the plane (gravity)
+            //kalman*gravity
+            u_projection_magnitude = (twoDX*findGX.outputHistory[0] + twoDY*findGY.outputHistory[0] + twoDZ*findGZ.outputHistory[0])/(Math.Pow(findGX.outputHistory[0], 2)+Math.Pow(findGY.outputHistory[0], 2)+Math.Pow(findGZ.outputHistory[0], 2));
+           //multiply gravity vector by u_projection_magnitude
+            orthogonal_x = u_projection_magnitude*findGX.outputHistory[0];
+            orthogonal_y = u_projection_magnitude*findGY.outputHistory[0];
+            orthogonal_z = u_projection_magnitude*findGZ.outputHistory[0];
+
+            //Remove the component of u that is orthogonal to the plane
+            compass_needle_x = twoDX - orthogonal_x;
+            compass_needle_y = twoDY - orthogonal_y;
+            compass_needle_z = twoDZ - orthogonal_z;
+
+
+            heading = Math.Atan2(compass_needle_y, compass_needle_x);
+            disp_data += string.Format("Compass at: {0:F4}, degrees!\n",heading);
+            
             // Update text on the screen
             stepCountViewer.GetComponent<UnityEngine.UI.Text>().text = disp_data;
-
-
 
 
 
@@ -129,7 +174,7 @@ public class AppManager : MonoBehaviour
             //     temp = 1;
             // }
             
-            data += "," + findGX.outputHistory[0]  + "," + findGY.outputHistory[0]  + "," + findGZ.outputHistory[0];
+            data += "," + Input.acceleration.x + "," + findGX.outputHistory[0]  + "," + findGY.outputHistory[0]  + ","+ Input.acceleration.y + "," + findGZ.outputHistory[0] + "," + Input.acceleration.z + ",";
             // data += "," + gyro_first_attempt.orientationX + "," + gyro_first_attempt.orientationY + "," + gyro_first_attempt.orientationZ + "," + compass.aboutX + "," + compass.aboutY + "," + compass.aboutZ + ",";
             // data += "," + Input.acceleration.x + "," + fbwX.outputHistory[0] + "," + temp + ",";
             dl.AppendData(data);
